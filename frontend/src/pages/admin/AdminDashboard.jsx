00955c79972f5e271e3f4e8e4241080a1aa12729
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2024-12-09 18:38:57 +0800

    ADD dashboard and directories

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 
 
-const Dashboard = () => {
+const AdminDashboard = () => {
     return (
         <div>
             <h1>Welcome </h1>
@@ -8,4 +8,4 @@ const Dashboard = () => {
     )
 }
 
-export default Dashboard
+export default AdminDashboard
---
+++ frontend/src/pages/student/StudentDashboard.jsx
@@ -0,0 +1,11 @@
+
+
+const StudentDashboard = () => {
+    return (
+        <div>
+            <h1>Welcome </h1>
+        </div>
+    )
+}
+
+export default StudentDashboard
